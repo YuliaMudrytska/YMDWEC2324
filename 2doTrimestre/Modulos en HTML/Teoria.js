a3b049252c5046e1303432564fb84f0db6e17b91
yuliaM <yuliaM@example.com>
2024-02-18 17:18:58 +0100

    18

--- 2doTrimestre/Modulos en HTML/Teoria.js
+++
@@ -1,7 +0,0 @@
-import { cadenaMayuscula } from "./Archivo.js";
-
-const saludar = cadenaMayuscula("¡hola, bienveni@!");
-console.log(saludar);
-
-import { NUEROPI, NUMEROCERO, NUMEROE } from "./Archivo.js"
-console.log(NUEROPI, NUMEROE, NUMEROCERO)
